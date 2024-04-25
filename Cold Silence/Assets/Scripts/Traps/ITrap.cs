4933cac3cb8a482078d2ed452701f59ad0218e06
ostrovnekich <nekichostrov@gmail.com>
2024-04-26 00:51:18 +0300

    Fixed power up

--- Cold Silence/Assets/Scripts/Traps/ITrap.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ITrap 
-{
-    public abstract void FallIntoTrap(TrapController trapController);
-}
